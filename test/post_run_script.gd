57f570bd0ca1aa5c023046e60471c69b06537940
James Ridley <rdljam003@cs.uct.ac.za>
2024-08-10 20:17:37 +0200

    disable unit tests in non-existent integration folder

--- test/post_run_script.gd
+++ test/post_run_script.gd
@@ -2,7 +2,4 @@ extends GutHookScript
 
 func run():
 	# Note, this will node will be included in the stray node list.
-	var n = Node.new()
-	n.name = "OrphanPrinter"
-	n.print_orphan_nodes()
-	n.free()
+	Node.print_orphan_nodes()
