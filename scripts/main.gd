712b034c7c0bb9c5ee1e844c946162c53a2d19c1
chipwayambokomas <chipwayambokomas@gmail.com>
2024-08-14 16:06:38 +0200

    Instantiated level class in main

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,16 @@
 extends Node2D
 
+@onready var level:PackedScene = preload("res://scenes/level/level.tscn")
+	
 # TODO: change so it will instance a level
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("hello world")
+	# Instantiate the loaded scene
+	var lvl = level.instantiate()
+	# Add the instance to the current scene
+	add_child(lvl)
+	# size farm, what state is the goal
+	#lvl.set_level()
+	
+	
+
