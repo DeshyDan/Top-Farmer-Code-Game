481fd7e9378e779df25dae74ed8dad8d54acf524
Deshy Dan <devdeshy@gmail.com>
2024-08-22 21:40:49 +0200

    Add trackpad zooming

--- scripts/camera/camera.gd
+++ scripts/camera/camera.gd
@@ -1,19 +1,19 @@
 extends Camera2D
 
 const MIN_ZOOM: float = 0.1
-const MAX_ZOOM: float = 1.0
+const MAX_ZOOM: float = 5.0
 const ZOOM_RATE: float = 8.0
 const ZOOM_INCREMENT: float = 0.1
 
-var _target_zoom: float = 1.0
+var target_zoom: float = 1.0
 
-var _tween: Tween 
+var tween: Tween 
 
 func _ready():
-	var _tween = get_tree().create_tween()
+	var tween = get_tree().create_tween()
 func _physics_process(delta: float) -> void:
-	zoom = lerp(zoom, _target_zoom * Vector2.ONE, ZOOM_RATE * delta)
-	set_physics_process(not is_equal_approx(zoom.x, _target_zoom))
+	zoom = lerp(zoom, target_zoom * Vector2.ONE, ZOOM_RATE * delta)
+	set_physics_process(not is_equal_approx(zoom.x, target_zoom))
 
 
 func _input(event):
@@ -26,16 +26,14 @@ func _unhandled_input(event: InputEvent) -> void:
 				zoom_in()
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 				zoom_out()
-	# TODO: Handle trackpad events
-
+	if event is InputEventMagnifyGesture:
+		zoom *= event.factor
 
 func zoom_in() -> void:
-	_target_zoom = max(_target_zoom - ZOOM_INCREMENT, MIN_ZOOM)
+	target_zoom = max(target_zoom - ZOOM_INCREMENT, MIN_ZOOM)
 	set_physics_process(true)
-
-
 func zoom_out() -> void:
-	_target_zoom = min(_target_zoom + ZOOM_INCREMENT, MAX_ZOOM)
+	target_zoom = min(target_zoom + ZOOM_INCREMENT, MAX_ZOOM)
 	set_physics_process(true)
 
 
