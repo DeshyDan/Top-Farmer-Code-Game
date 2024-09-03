0001267712da87893233481d5cc38f0f2c9679dd
James Ridley <rdljam003@cs.uct.ac.za>
2024-09-03 15:39:35 +0200

    integrate autocomplete with dev changes

--- scripts/code_editor/player_code_edit.gd
+++ scripts/code_editor/player_code_edit.gd
@@ -5,7 +5,8 @@ extends CodeEdit
 
 @export var executing_color: Color
 @export var executing_highlight_length: float = 1
-@export var comment_color: Color
+
+@export var error_box: Control
 
 var _background_color: Color
 var _tweening_lines = []
@@ -15,7 +16,7 @@ func _init():
 	var keywords = JSON.parse_string(keyword_data)
 	for keyword in keywords["list"]:
 		syntax_highlighter.keyword_colors[keyword] = Color.LIGHT_CORAL
-		add_code_completion_option(CodeEdit.KIND_MEMBER, keyword, keyword)
+		add_code_completion_option(CodeEdit.KIND_MEMBER, keyword,keyword)
 
 func _ready():
 	_background_color = get_theme_color("background_color")
@@ -35,14 +36,24 @@ func highlight_line(lineno):
 		executing_highlight_length
 		).set_ease(Tween.EASE_IN)
 
-func _process(delta):
-	pass
+func draw_error(lineno, colno, raw_message):
+	set_line_background_color(lineno-1, Color.RED * 0.4)
+	set_caret_column(colno)
+	set_caret_line(lineno - 1)
+	
+	error_box.position = get_caret_draw_pos()
+	# get_caret_draw_pos() is bugged so have to correct it:
+	error_box.position.y -= get_line_height()
+	
+	error_box.set_text(raw_message)
+	error_box.visible = true
 
-func draw_error(lineno, colno, length):
-	pass
 
 func _on_text_changed():
 	request_code_completion()
+	error_box.visible = false
+	for line in get_line_count():
+		set_line_background_color(line, _background_color)
 
 func _on_code_completion_requested():
 	MessageBus.request_code_completion(text)
