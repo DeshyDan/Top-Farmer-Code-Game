43dc8317f3af20e3c712c0d3937e222dd3048d8f
Deshy Dan <devdeshy@gmail.com>
2024-09-06 13:13:10 +0200

    Clean up test

--- test/integration/farm/test_farm_view.gd
+++ test/integration/farm/test_farm_view.gd
@@ -1,136 +1,74 @@
 extends GutTest
 
 @onready var level_scene:PackedScene = preload("res://scenes/level/level.tscn")
-var level: Node
 
-const WIDTH:int = 6
-const HEIGHT:int = 6
-const ROCK_COORDS = [Vector2i(0,2),Vector2i(3,2), Vector2i(5, 3)]
-const WATER_COORDS = [Vector2i(1,3), Vector2i(1,4), Vector2i(2, 3), Vector2i(2, 4)]
+var level: Node
 var farm_view:FarmView
-var tick_count_unoptimized=0
 
 const TICK_RATE = 30
 
 const HARVEST_LEVEL = "res://test/test_levels/farm_view_testers/test_harvesting.txt"
 const MOVE_LEVEL = "res://test/test_levels/farm_view_testers/test_robot_movement.txt"
 const PLANT_LEVEL = "res://test/test_levels/farm_view_testers/test_planting.txt"
+
 const MATURE_HARVEST_SCRIPT = "res://test/test_scripts/farm/harvest.txt"
 const PREMATURE_HARVEST_SCRIPT = "res://test/test_scripts/farm/premature_harvest.txt"
 const PLANT_SCRIPT = "res://test/test_scripts/farm/plant.txt"
 const MOVE_SCRIPT = "res://test/test_scripts/farm/move.txt"
-# vis representation of farm at start
-
 
 func before_each():
-	#var farm_model = farm_model_generator()
-	#var farm_view = FarmView.new()
-	#farm_view.plot_farm(farm_model)
 	level = level_scene.instantiate() as Level
 	add_child(level)
 	
-func set_level(file_path:String):
-	var level_skeleton = FileAccess.open(file_path, FileAccess.READ)
+func setup_level(level_skeleton_file_path:String,test_script:String):
+	var level_skeleton = FileAccess.open(level_skeleton_file_path, FileAccess.READ)
 	
 	var goal_harvest = {
 		Const.PlantType.PLANT_CORN: 4, 
 		Const.PlantType.PLANT_GRAPE:4
 	}
 	level.set_level(level_skeleton, goal_harvest)
+	watch_signals(level.farm)
+	level = level as Level
+	var source = FileAccess.get_file_as_string(test_script)
+
+	level.window.code_edit.text = source
+	level.tick_rate = TICK_RATE 
+	
 	
 func after_each():
 	remove_child(level)
 	level.free()
 	
-func farm_model_generator():
-	var farm_model = FarmModel.new(WIDTH, HEIGHT)
-	for i in ROCK_COORDS:
-		assert_true(farm_model.is_empty(i))
-		farm_model.add_farm_item(Obstacle.ROCK(), i)
-		
-		assert_false(farm_model.is_empty(i))
-		assert_true(farm_model.is_obstacle(i))
-	
-	for i in WATER_COORDS:
-		assert_true(farm_model.is_empty(i))
-		farm_model.add_farm_item(Obstacle.WATER(), i)
-		
-		assert_false(farm_model.is_empty(i))
-		assert_true(farm_model.is_obstacle(i))
-	
-	
-	return farm_model
-	
-
 func test_plant():
-	set_level(PLANT_LEVEL)
-	watch_signals(level.farm)
-	level = level as Level
-	
-	var source = FileAccess.get_file_as_string(PLANT_SCRIPT)
-	
-	
-	level.window.code_edit.text = source
-	level.tick_rate = TICK_RATE 
+	setup_level(PLANT_LEVEL, PLANT_SCRIPT)
+
 	
 	level._on_window_run_button_pressed()
-	#level.interpreter_client.load_source(level.window.get_source_code())
-	#level.interpreter_client.start()
-	#
-	#level._on_timer_tick()
-	#level._on_timer_tick()
-	#level._on_timer_tick()
-	#level._on_timer_tick()
 
 	assert_signal_emitted_with_parameters(level.farm,"plant_completed", [true],0)
 	await yield_to(level.farm, "plant_completed",10)
 	assert_signal_emitted_with_parameters(level.farm,"plant_completed", [false],1)
-	
-	#assert_signal_emitted_with_parameters(level.farm,"plant_completed", [false],1)
 
 func test_premature_harvest():
-	set_level(HARVEST_LEVEL)
-	watch_signals(level.farm)
-	level = level as Level
-	
-	var source = FileAccess.get_file_as_string(PREMATURE_HARVEST_SCRIPT)
-	
-	
-	level.window.code_edit.text = source
-	level.tick_rate = TICK_RATE 
-	
+	setup_level(HARVEST_LEVEL,PREMATURE_HARVEST_SCRIPT)
+
 	level._on_window_run_button_pressed()
 	
 	await yield_to(level.farm, "harvest_completed",5)
 	assert_signal_emitted_with_parameters(level.farm,"harvest_completed", [false],0)
 	
 func test_mature_harvest():
-	set_level(HARVEST_LEVEL)
-	watch_signals(level.farm)
-	level = level as Level
-	
-	var source = FileAccess.get_file_as_string(MATURE_HARVEST_SCRIPT)
-	
-	
-	level.window.code_edit.text = source
-	level.tick_rate = TICK_RATE 
-	
+	setup_level(HARVEST_LEVEL, MATURE_HARVEST_SCRIPT)
+
 	level._on_window_run_button_pressed()
 	
 	await yield_to(level.farm, "harvest_completed",15)
 	assert_signal_emitted_with_parameters(level.farm,"harvest_completed", [true],0)
 
 func test_move():
-	set_level(MOVE_LEVEL)
-	watch_signals(level.farm)
-	level = level as Level
-	
-	var source = FileAccess.get_file_as_string(MOVE_SCRIPT)
-	
-	
-	level.window.code_edit.text = source
-	level.tick_rate = TICK_RATE
-	
+	setup_level(MOVE_LEVEL, MOVE_SCRIPT)
+
 	level._on_window_run_button_pressed()
 	
 	await yield_to(level.interpreter_client, "finished",15)
@@ -145,18 +83,10 @@ func test_move():
 	assert_signal_emitted_with_parameters(level.farm,"move_completed", [false],5) # RIGHT |WATER
 	assert_signal_emitted_with_parameters(level.farm,"move_completed", [true],6) # UP | LAND 
 	assert_signal_emitted_with_parameters(level.farm,"move_completed", [false],7) # RIGHT |ROCK
-	
 
 func test_harvestables():
-	set_level(PLANT_LEVEL)
-	level = level as Level
-	
-	var source = FileAccess.get_file_as_string(MATURE_HARVEST_SCRIPT)
-	
-	
-	level.window.code_edit.text = source
-	level.tick_rate = TICK_RATE
-	
+	setup_level(PLANT_LEVEL, MATURE_HARVEST_SCRIPT)
+
 	assert_eq(level.farm.get_harvestables(), {})
 	
 	level._on_window_run_button_pressed()
@@ -170,30 +100,19 @@ func test_harvestables():
 			Const.PlantType.PLANT_GRAPE:
 				assert_true(level.farm.get_harvestables()[i] == 2)
 	
-	
 func test_reset():
-	set_level(PLANT_LEVEL)
-	level = level as Level
-	
-	var source = FileAccess.get_file_as_string(MATURE_HARVEST_SCRIPT)
-	
-	
-	level.window.code_edit.text = source
-	level.tick_rate = TICK_RATE 
-	
-	
-	
+	setup_level(PLANT_LEVEL, MATURE_HARVEST_SCRIPT)
+
 	level._on_window_run_button_pressed()
 	
 	await yield_to(level.interpreter_client, "finished",15)
 	
-	source = FileAccess.get_file_as_string("")
+	var source = FileAccess.get_file_as_string("")
 	level.window.code_edit.text = source
 	level._on_window_run_button_pressed()
 	
-	# Harvestables are reset
 	assert_eq(level.farm.get_harvestables(), {})
-	# farm model is clear
+
 	for i in level.farm.farm_model.get_data():
 		assert_true((i is Obstacle) or (i == null))
 
