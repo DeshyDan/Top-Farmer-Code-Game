cce12b5cc0940afdfe4839427f3a4faab49ceeee
Deshy Dan <devdeshy@gmail.com>
2024-09-06 12:57:00 +0200

    Test farm model

--- test/unit/farm/test_farm_model.gd
+++ test/unit/farm/test_farm_model.gd
@@ -8,17 +8,56 @@ var farm_model:FarmModel
 func before_each():
 	farm_model = FarmModel.new(WIDTH, HEIGHT)
 
-func test_empty_when_no_farm_item():
-	assert_eq(farm_model.is_empty(Vector2i(0,0)), true)
+func test_is_empty():
+	var coord = Vector2i(0,0)
+	assert_true(farm_model.is_empty(coord))
+	farm_model.add_farm_item(FarmItem.new(0,0), coord)
+	assert_false(farm_model.is_empty(coord))
 
-func test_empty_when_there_is_farm_item():
-	farm_model.add_farm_item(FarmItem.new(0,0), Vector2i(0,0))
-	assert_eq(farm_model.is_empty(Vector2i(0,0)), false)
 
-func test_add_farm_tem():
-	farm_model.add_farm_item(FarmItem.new(0,0), Vector2i(0,0))
-func test_height():
+func test_add_farm_item():
+	var farm_items = FarmItem.new(0,0)
+	farm_model.add_farm_item(farm_items, Vector2i(0,0))
+	
+	for i in range(WIDTH * HEIGHT):
+		if (i ==0 ):
+			assert_eq(farm_model.get_data()[i], farm_items)
+			continue
+		assert_null(farm_model.get_data()[i])
+			
+func test_farm_dimensions():
 	assert_eq(farm_model.get_height(), HEIGHT)
-
-func test_get_width():
 	assert_eq(farm_model.get_width(), WIDTH)
+
+func test_is_harvestable():
+	var coord = Vector2i(0,0)
+
+	farm_model.add_farm_item(Plant.new(0,0,0), coord)
+	
+	assert_false(farm_model.is_harvestable(coord))
+	
+	farm_model.set_harvestable(coord)
+	
+	assert_true(farm_model.is_harvestable(coord))
+
+func test_is_obstacle():
+	var obstacle_coord = Vector2i(0,0)
+	var plant_coord = Vector2i(0,1)
+	
+	farm_model.add_farm_item(Obstacle.new(0,0,0), obstacle_coord)
+	assert_true(farm_model.is_obstacle(Vector2i(0,0)))
+	
+	farm_model.add_farm_item(Plant.new(0,0,0), plant_coord)
+	assert_false(farm_model.is_obstacle(plant_coord))
+
+func test_remove():
+	var coord = Vector2i(0,0)
+	
+	farm_model.add_farm_item(Plant.new(0,0,0), coord)
+	
+	assert_eq(farm_model.is_empty(coord), false)
+	
+	farm_model.remove(coord)
+	
+	assert_eq(farm_model.is_empty(coord), true)
+
