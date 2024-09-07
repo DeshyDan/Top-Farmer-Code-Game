6b54042d227eda62540a55c4492bde65dc8130d5
chipwayambokomas <chipwayambokomas@gmail.com>
2024-09-07 14:19:19 +0200

    updated run test

--- test/integration/test_demo.gd
+++ test/integration/test_demo.gd
@@ -12,14 +12,11 @@ func before_each():
 	
 	var level = 4
 	
-	var file_path = "res://assets/levels/lvl_"+str(level)+".txt"
-	var lvl_skeleton = FileAccess.open(file_path, FileAccess.READ)
-	
 	var goal_harvest = {
 		Const.PlantType.PLANT_CORN: 4
 	}
 	
-	lvl.set_level(lvl_skeleton, goal_harvest,level)
+	lvl.set_level(goal_harvest,level)
 
 func test_demo_unoptimized():
 	lvl = lvl as Level
