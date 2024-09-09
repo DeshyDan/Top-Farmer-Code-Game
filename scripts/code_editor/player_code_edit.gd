3132b8d009eedff8747dab15b2f3171e23806bce
James Ridley <rdljam003@cs.uct.ac.za>
2024-09-09 11:37:55 +0200

    replace mistake highlighter with red highlighted line

--- scripts/code_editor/player_code_edit.gd
+++ scripts/code_editor/player_code_edit.gd
@@ -7,10 +7,9 @@ extends CodeEdit
 @export var comment_color: Color
 @export var executing_highlight_length: float = 1
 
-@export var error_box: Control
-
 var _background_color: Color
 var _tweening_lines = []
+var _error_line = -1
 
 # Called when the node enters the scene tree for the first time.
 func _init():
@@ -24,7 +23,9 @@ func _ready():
 	_background_color = get_theme_color("background_color")
 	for key in Const.DEFAULT_BUILTIN_CONSTS:
 		syntax_highlighter.keyword_colors[key] = Color.AQUAMARINE
-	syntax_highlighter.add_color_region("#","", comment_color)
+	
+	if not syntax_highlighter.has_color_region("#"):
+		syntax_highlighter.add_color_region("#","", comment_color)
 	
 	# have to do this otherwise random godot functions show up
 	update_code_completion_options(true)
@@ -34,6 +35,8 @@ func highlight_line(lineno):
 	var tween = create_tween()
 	tween.tween_method(
 		func (color):
+			if lineno == _error_line:
+				return
 			set_line_background_color(lineno-1,color), 
 		executing_color, 
 		_background_color, 
@@ -41,20 +44,12 @@ func highlight_line(lineno):
 		).set_ease(Tween.EASE_IN)
 
 func draw_error(lineno, colno, raw_message):
-	set_line_background_color(lineno-1, Color.RED * 0.4)
-	set_caret_column(colno)
-	set_caret_line(lineno - 1)
-	
-	error_box.position = get_caret_draw_pos()
-	# get_caret_draw_pos() is bugged so have to correct it:
-	error_box.position.y -= get_line_height()
-	
-	error_box.set_text(raw_message)
-	error_box.visible = true
+	set_line_background_color(lineno - 1, Color.RED * 0.4)
+	_error_line = lineno
 
 func _on_text_changed():
 	request_code_completion()
-	error_box.visible = false
+	_error_line = -1
 	for line in get_line_count():
 		set_line_background_color(line, _background_color)
 
