7186114b3db9176ca08e38a68cabf28a2ea18b77
James Ridley <rdljam003@cs.uct.ac.za>
2024-09-09 14:54:55 +0200

    remove commented out code

--- autoload/message_bus.gd
+++ autoload/message_bus.gd
@@ -3,13 +3,6 @@ extends Node
 
 signal code_completion_requested(source: String)
 signal code_completion_set(options: Array[CodeCompletionOption])
-#signal interpreter_input(data: Variant)
-
-#func send_input(data: Variant):
-	#_send_input.call_deferred(data)
-#
-#func _send_input(data: Variant):
-	#interpreter_input.emit(data)
 
 func request_code_completion(source: String):
 	code_completion_requested.emit(source)
