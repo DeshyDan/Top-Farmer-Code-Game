6c6da95b4feef814637016df1e84a46614a46633
Deshy Dan <devdeshy@gmail.com>
2024-09-15 19:55:42 +0200

    Test load level select screen from main

--- test/integration/main_menu/test_main.gd
+++ test/integration/main_menu/test_main.gd
@@ -26,3 +26,14 @@ func test_load_main_screen():
 	
 	assert_false(main.level_select.visible)
 	assert_false(main.level_node.visible)
+
+func test_load_level_select_screen_from_main():
+	main.main_menu._on_play_button_pressed()
+	
+	assert_false(main.main_menu.visible)
+	assert_true(main.level_select.visible)
+	assert_false(main.level_node.visible)
+	
+	## Optimistic approach -> There will always be a level. It a game after all...actaully don't know what I'm doing LOL
+	assert_true(main.level_select.level_buttons.get_child_count() == 10)
+	
